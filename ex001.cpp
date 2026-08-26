1dd64cc27fa5fe8e2f916a697b1dbbd10020e901
Jota <joaopedrosbgonc@gmail.com>
2026-08-26 16:34:12 -0300

    melhoria do código

--- ex001.cpp
+++ ex001.cpp
@@ -3,21 +3,13 @@
 #include <stdio.h>
 
 int main(){
-	int n1, n2, n3, n4, i=1, soma;
-	
-	printf("Escreva o primeiro numero:\n--> ");
-	scanf("%d", &n1);
-	
-	printf("Escreva o primeiro numero:\n--> ");
-	scanf("%d", &n2);
-	
-	printf("Escreva o primeiro numero:\n--> ");
-	scanf("%d", &n3);
-	
-	printf("Escreva o primeiro numero:\n--> ");
-	scanf("%d", &n4);
-	
-	soma = n1 + n2 + n3 + n4;
-	printf("A soma dos 4 numeros e: %d", soma);
-	
+	int soma = 0, i, num;
+
+	for(i=1;i<=4;i++){
+		printf("Escreva o %dº numero\n--> ", i);
+		scanf("%d", &num);
+		soma = soma + num;
+		num = 0;
+	}
+	printf("%d", soma);
 }
---
+++ ex003.cpp
@@ -0,0 +1,7 @@
+//Faça um programa que receba três notas e seus respectivos pesos, calcule e mostre a média ponderada dessas notas
+
+#include <stdio.h>
+
+int main(){
+	
+}
