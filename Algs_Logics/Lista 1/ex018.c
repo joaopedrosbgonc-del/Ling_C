8a31cba0888021a624966e539ee4a731fc827d80
Jota <joaopedrosbgonc@gmail.com>
2026-08-30 11:47:53 -0300

    18

--- Algs_Logics/Lista 1/ex018.c
+++ Algs_Logics/Lista 1/ex018.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 
 int main(){
-	float pesoS, Qnt, cadaG, tempo;
+	float pesoS, Qnt, cadaG, gasto, resto;
 	
-	printf("Qual o peso do saco e a quantidade para cada gato? (g) [x y]\n--> ");
+	printf("Qual o peso do saco e a quantidade para cada gato? (Kg)(g) [x y]\n--> ");
 	scanf("%f %f", &pesoS, &Qnt);
 	
-	cadaG = Qnt / 1000;
-	tempo = pesoS / cadaG;
-	
-	printf("%.0f dias para acabar", tempo);
+	cadaG = Qnt / 1000.0;
+	gasto = (cadaG * 2) * 5;
+
+	resto = pesoS - gasto;
+	printf("Ao final de 5 dias, sobra %.1f Kg", resto);
 }
